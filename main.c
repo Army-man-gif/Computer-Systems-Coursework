f8771c7aea11329aa1bbe6886e124ff84777067c
JoshS2005 <josh.s2005@hotmail.com>
2024-11-21 13:30:05 +0000

    test2

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     printf("Hi guysssssssssss");
-    printf("Hello world yipeee");
+    printf("test");
     printf("Changing code directly in gitHub to simulate another collaborator commiting changes");
     return 0;
 }
