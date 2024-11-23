389276987047ae65ccb1bb764eeab9341a367a9d
Armaan Khaitan <khaitanarmaan@gmail.com>
2024-11-23 18:22:22 +0000

    Added number of lines and quit function

--- main.c
+++ main.c
@@ -25,27 +25,40 @@ End of file
 #include<stdio.h>
 #include<string.h>
 
-void displayFile(const char *filePath){
+void numberOfLines(const char *filePath){
     FILE *openedFile;
     int number = 0;
     char c;
     openedFile = fopen(filePath,"r");
     if(openedFile == NULL){
+        printf("\n");
+        printf("Unable to open file");
+    }else{
+        printf("\n");
+        while(c!=EOF){
+            c = fgetc(openedFile);
+            if(c=='\n'){
+                number += 1;
+            }
+        }
+        printf("Lines: %d ",number+1);
+    }
+}
+void displayFile(const char *filePath){
+    FILE *openedFile;
+    char c;
+    openedFile = fopen(filePath,"r");
+    if(openedFile == NULL){
+        printf("\n");
         printf("Unable to open file");
     }else{
         printf("\n");
         printf("File contents: \n");
-        while(1){
+        while(c!=EOF){
             c = fgetc(openedFile);
-            if(c==EOF){
-                break;
-            }else{
-                number += 1;
-                printf("%c",c);
-            }
+            printf("%c",c);
         }
         printf("\n");
-        printf("Number of Line: \n "+number);
         fclose(openedFile);
     }
 
@@ -54,6 +67,7 @@ void writeLine(const char *filePath){
     FILE *openedFile;
     openedFile = fopen(filePath,"a");
     if(openedFile == NULL){
+        printf("\n");
         printf("Unable to open file");
     }else{
         fprintf(openedFile,"\n");
@@ -63,6 +77,7 @@ void writeLine(const char *filePath){
 
 void command(){
     char com[50];
+    char f[] = "D:\\Computer systems cwk\\Project\\file.txt";
     printf("\n");
     printf("Enter command: ");
     fgets(com,sizeof(com),stdin);
@@ -70,10 +85,13 @@ void command(){
     com[strcspn(com,"\n")] = '\0';
     if(strcmp(com,"Quit")!=0){
         if(strcmp(com,"Append Line")==0){
-            char f[] = "D:\\Computer systems cwk\\Project\\file.txt";
             writeLine(f);
             printf("\n");
             printf("Writing successful");
+            displayFile(f);
+        }else if(strcmp(com,"Lines")==0){
+            numberOfLines(f);
+        }else if(strcmp(com,"Show")==0){
             displayFile(f);
         }else{
             printf("\n");
@@ -85,10 +103,9 @@ void command(){
     }
 }
 int main(){
-    char com[] = "Commands: \n Append Line: \n Adds new line \n Quit: \n Stops program";
+    char com[] = "Commands: \n Append Line: \n Adds new line \n Quit: \n Stops program \n Show: \n Shows file contents \n Lines: \n Number of lines in file \n";
     printf("%s",com);
     char f[] = "D:\\Computer systems cwk\\Project\\file.txt";
-    displayFile(f);
     command();
     return 0;
 
