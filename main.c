2d79118c477ad8775243c95ffb3bc15a187079e3
Armaan Khaitan <khaitanarmaan@gmail.com>
2024-11-28 18:29:13 +0000

    Done simplified version of custom function, only left to fix insert and delete and complexify custom function

--- main.c
+++ main.c
@@ -73,7 +73,7 @@ void deleteLine(int Line) {
     }else{
         int totalcharacters = NumberOfCharactersTillSpecificLine(numberOfLines());
         char buffer[totalcharacters+1];
-        int index = 0;
+        int index = 0                ;
         int count = 1;
         printf("\n");
         while(c!=EOF){
@@ -100,8 +100,7 @@ void deleteLine(int Line) {
 void showLine(int Line){
     FILE *openedFile = fopen(f,"r");
     fseek(openedFile,NumberOfCharactersTillSpecificLine(Line),SEEK_SET);
-    char c = fgetc(openedFile);
-    fseek(openedFile,NumberOfCharactersTillSpecificLine(Line),SEEK_SET);
+    char c = '0';
     while (c != EOF && c != '\n') {
         c = fgetc(openedFile);
         printf("%c",c);
@@ -135,7 +134,7 @@ void writeLine(const char *value){
             printf("\n");
             printf("Unable to open file");
         }else{
-            fprintf(openedFile,value);
+            fprintf(openedFile,"\n %s",value);
             fclose(openedFile);
         }
     }else{
@@ -143,7 +142,40 @@ void writeLine(const char *value){
         fclose(openedFile);
     }
 }
-void insertLine(){
+void insertLine(int Line,const char *value){
+    FILE *openedFile = fopen(f,"r");
+    fseek(openedFile,NumberOfCharactersTillSpecificLine(Line),SEEK_SET);
+    char c = '0';
+    int count = 0;
+    char before_insert_buffer[NumberOfCharactersTillSpecificLine(numberOfLines())+strlen(value)+1];
+    for(int i=0;i<Line;i++){
+        while(c!=EOF && c!='\n'){
+            c = fgetc(openedFile);
+            before_insert_buffer[count]=c;
+            count++;
+        }
+        
+        before_insert_buffer[count]='\n';
+        count++;
+    }
+    printf("%s",before_insert_buffer);
+    strcpy(&before_insert_buffer[count], value);
+    count += strlen(value);
+    before_insert_buffer[count]='\n';
+    count++;
+    for(int i=Line;i<numberOfLines();i++){
+        while(c!=EOF && c!='\n'){
+            c = fgetc(openedFile);
+            before_insert_buffer[count]=c;
+            count++;
+        }
+        before_insert_buffer[count]='\n';
+        count++;
+    }
+    fclose(openedFile);
+    openedFile = fopen(f,"w");
+    fseek(openedFile,0,SEEK_SET);
+    fprintf(openedFile,before_insert_buffer);
 
 }
 char **AddToLog(char **strings,int original_size,const char *addedLog){
@@ -165,6 +197,8 @@ void showChangeLog(char **strings,int size){
 }
 void command(char **log,int size){
     char com[50];
+    char value[100];
+    char insertPhrase[100];
     int con;
     printf("Enter command: ");
     fgets(com,sizeof(com),stdin);
@@ -172,9 +206,10 @@ void command(char **log,int size){
     com[strcspn(com,"\n")] = '\0';
     if(strcmp(com,"Quit")!=0){
         if(strcmp(com,"Append Line")==0){
-            char value[100];
             printf("Enter phrase to be appended: \n");
             fgets(value,sizeof(value),stdin);
+            value[strcspn(value, "\n")] = '\0';
+            printf("%s",value);
             writeLine(value);
             printf("\n");
             printf("Writing successful");
@@ -223,6 +258,24 @@ void command(char **log,int size){
             fflush(stdin);
             log = AddToLog(log,size,"Showed line \n");
             size++;
+        }else if(strcmp(com,"Insert Line")==0){
+            int line = 0;
+            con = 0;
+            do{
+                printf("Enter the line you want to insert at: \n");
+                scanf("%d",&line);
+                if(line>numberOfLines() || line<1){
+                    con = 1;
+                    printf("Line number out of range \n");
+                }
+
+            }while(con==1);
+            printf("Enter phrase to be inserted: \n");
+            fgets(insertPhrase,sizeof(insertPhrase),stdin);
+            value[strcspn(value, "\n")] = '\0';
+            insertLine(line,insertPhrase);
+            log = AddToLog(log,size,"Inserted line \n");
+            size++;
         }else if(strcmp(com,"Show Change Log")==0){
             log = AddToLog(log,size,"Showed change log \n ");
             size++;
@@ -244,7 +297,7 @@ int main(){
     strings = (char**)malloc(size * sizeof(char*));
     AddToLog(strings,size,"Original unchanged file");
     showChangeLog(strings,size);
-    char com[] = "Commands:  \nShow Change Log \n Shows change log \n Show Line: \n Show content on specified line \n Append Line: \n Adds new line \n Quit: \n Stops program \n Show: \n Shows file contents \n Delete Line \n Deletes a line that you will get to specify after this command \n Lines: \n Number of lines in file \n";
+    char com[] = "Commands:  \nInsert Line \n Insert line of specific content at speocific line \n Show Change Log \n Shows change log \n Show Line: \n Show content on specified line \n Append Line: \n Adds new line \n Quit: \n Stops program \n Show: \n Shows file contents \n Delete Line \n Deletes a line that you will get to specify after this command \n Lines: \n Number of lines in file \n";
     printf("%s",com);
     //move("C:/Users/khait/Downloads/file.txt",f);
     command(strings,size);
